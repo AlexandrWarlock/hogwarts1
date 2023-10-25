f7e48811dbca9035d4b10a89242d1ea2f0997b7d
AlexandrWarlock <alexandrpetrov291094@gmail.com>
2023-10-25 12:52:14 +0300

    1

---
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -0,0 +1,12 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Faculty;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class FacultyService {
+    Map<Long, Faculty> Faculties = new HashMap<>();
+}
---
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -0,0 +1,12 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Student;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class StudentService {
+    Map<Long, Student> Students = new HashMap<>();
+}
